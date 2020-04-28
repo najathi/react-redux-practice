ba32c1d728bc18585f8ffafa7c23c0e3da81c065
najathi <najathi777@gmail.com>
2020-04-28 14:18:40 +0530

    redux basic in node js way

---
+++ redux-basics.js
@@ -0,0 +1,38 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+// Reducer
+const initialState = {
+	counter: 0
+}
+
+const rootReducer = (state = initialState, action) => {
+	if (action.type === 'INC_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + 1
+		}
+	}
+	if (action.type === 'ADD_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + action.value
+		}
+	}
+	return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Subscription - (subscribe takes an argument a function which will be executed when ever the state is updated, So whenever an action reached the reducer, Subscribe is triggering whenever update the state)
+store.subscribe(() => {
+	console.log('[Subscription]', store.getState())
+});
+
+// Dispatching Action (sending the data to state)
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
